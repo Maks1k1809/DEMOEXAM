0d3c36f88c119477c157cccd1e80ebe4bbc525f3
Пользователь <@DESKTOP-TMA>
2023-11-23 11:44:20 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using TarasovMaximDemo.Entity;
+using TarasovMaximDemo.Properties;
+
+namespace TarasovMaximDemo
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Entity.TarasovEntities Context { get; set; } = new TarasovEntities();
+        public static User AuthUser
+        {
+            get
+            {
+                return Context.User.Find(Settings.Default.UserID);
+            }
+        }
+
+        public App()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            MessageBox.Show("Произошла непредвиденная ошибка в работе программы, проверьте подключение к базе данных. Если ошибка появится снова - обратитесь к вашему системному администратору.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information); ;
+        }
+    }
+}
